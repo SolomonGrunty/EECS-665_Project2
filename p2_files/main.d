ea69a167245c5ce2c77a6fdb904f2bb5420186dc
Miller Bath <millerzbath@gmail.com>
2020-09-12 18:57:59 -0500

    clean setup for will

--- p2_files/main.d
+++
@@ -1,9 +0,0 @@
-main.o: main.cpp errors.hpp scanner.hpp grammar.hh tokens.hpp
-
-errors.hpp:
-
-scanner.hpp:
-
-grammar.hh:
-
-tokens.hpp:
--- p2_files/parser.d
+++
@@ -1,9 +0,0 @@
-parser.o: parser.cc grammar.hh tokens.hpp scanner.hpp errors.hpp
-
-grammar.hh:
-
-tokens.hpp:
-
-scanner.hpp:
-
-errors.hpp:
